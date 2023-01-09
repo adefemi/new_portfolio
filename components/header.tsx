dcb5838f4b4b54919583a90a48e19a106f9b2501
Adefemi Oseni <adefemigreat1995@gmail.com>
2023-01-09 12:14:01 +0100

    implemented mobile view

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link"
+import { useState } from "react"
+import { MenuClose, MenuOpen } from "./svgs"
 
-const Header = ({active, isDark}: {active: string, isDark?: boolean}) => {
-
+const Header = ({ active, isDark }: { active: string, isDark?: boolean }) => {
+    const [menuOpen, setMenuOpen] = useState(false)
     const menuItems = [
         {
             name: "Home",
@@ -27,9 +29,15 @@ const Header = ({active, isDark}: {active: string, isDark?: boolean}) => {
 
     return <div className={`header ${isDark ? 'dark' : ''}`}>
         <Logo />
-        <div className="menu">
-            {menuItems.map((item, index) => <MenuItem {...item} key={index} active={active}/>)}
+        <div className="menu mobile">
+            {menuItems.map((item, index) => <MenuItem {...item} key={index} active={active} />)}
         </div>
+        <div className="menu-mobile desktop" onClick={() => setMenuOpen(true)}>
+            <MenuOpen />
+        </div>
+        {
+            menuOpen && <MobileMenu setMenuOpen={setMenuOpen} menuItems={menuItems} active={active} />
+        }
     </div>
 }
 
@@ -39,8 +47,22 @@ export const Logo = () => (
     <div className="logo">Adefemigreat</div>
 )
 
-export const MenuItem = ({name, linkTo, active}:{name:string, linkTo: string, active: string}) => (
+export const MenuItem = ({ name, linkTo, active }: { name: string, linkTo: string, active: string }) => (
     <div className={`menu-item ${linkTo === active ? 'active' : ''}`}>
         <Link href={linkTo}>{name}</Link>
     </div>
 )
+
+const MobileMenu = ({ setMenuOpen, menuItems, active }: { setMenuOpen: (i: boolean) => void, menuItems: any[], active: string }) => {
+    return <div className="mainMobileMenu desktop">
+        <div className="head-content">
+            <Logo />
+            <div className="menu-mobile" onClick={() => setMenuOpen(false)}>
+                <MenuClose />
+            </div>
+        </div>
+        <div className="menu">
+            {menuItems.map((item, index) => <MenuItem {...item} key={index} active={active} />)}
+        </div>
+    </div>
+}
